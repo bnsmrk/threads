d1f25b04548dc47aaf46930d892f231f9e3110f7
vinstech14 <vinsmark.adlawan0214@gmail.com>
2025-06-04 09:13:29 +0800

    supabase config

--- src/app/(protected)/new.tsx
+++ src/app/(protected)/new.tsx
@@ -1,28 +1,49 @@
 import { useState } from "react";
-import { Pressable, Text, TextInput, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function NewPostScreen() {
   const [text, setText] = useState("");
   return (
     <SafeAreaView className="p-4 flex-1">
-      <Text className="text-white text-lg font-bold">username</Text>
-      <TextInput
-        value={text}
-        onChangeText={setText}
-        placeholder="Whats on your mind?"
-        className="text-white text-lg"
-        multiline
-        numberOfLines={4}
-      />
-
-      <View className="mt-auto">
-        <Pressable
-          onPress={() => console.log("post: ", text)}
-          className="bg-white p-3 px-6 self-end rounded-full"
-        >
-          <Text className="text-black font-bold">Post</Text>
-        </Pressable>
-      </View>
+      <KeyboardAvoidingView
+        className="flex-1"
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 140 : 0}
+      >
+        <Text className="text-white text-lg font-bold">username</Text>
+        <TextInput
+          value={text}
+          onChangeText={setText}
+          placeholder="Whats on your mind?"
+          placeholderTextColor="gray"
+          className="text-white text-lg"
+          multiline
+          numberOfLines={4}
+        />
+        <TextInput
+          placeholder="Password"
+          placeholderTextColor="gray"
+          className="text-white text-lg"
+          multiline
+          numberOfLines={4}
+          secureTextEntry
+        />
+        <View className="mt-auto">
+          <Pressable
+            onPress={() => console.log("post: ", text)}
+            className="bg-white p-3 px-6 self-end rounded-full"
+          >
+            <Text className="text-black font-bold">Post</Text>
+          </Pressable>
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
+//import { AppState } from "react-native";
+
+const supabaseUrl = process.env.EXPO_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing Supabase URL or Anon Key");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+    // lock: processLock,
+  },
+});
+
+// AppState.addEventListener("change", (state) => {
+//   if (state === "active") {
+//     supabase.auth.startAutoRefresh();
+//   } else {
+//     supabase.auth.stopAutoRefresh();
+//   }
+// });
